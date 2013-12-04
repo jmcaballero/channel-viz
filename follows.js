02252aafe37090d32b9e53e5509fb367a30b4150
jmcaballero <jmcaballero@eresmas.net>
2013-12-04 16:00:59 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,10 +14,10 @@
 
 	var defaultKey		= 'tUjBOZmgR3cbyct0HKi0rD80PouMcmoxGg5cUK9lzbIF5FBg', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [957558440], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Jose Maria Caballero', // Replaces Xively logo in the header
+		applicationName	= 'TAMIC, S.L.', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= 'FFFFFF', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
